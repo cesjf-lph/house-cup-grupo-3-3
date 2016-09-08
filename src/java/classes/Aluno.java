98d4f9764a2f84dd75774d415ffcce0a92f25961
Anderson <anderson.clementesi@gmail.com>
2016-09-08 20:03:47 -0300

    Tirando alguns imports não utilizados para testar commit.

--- src/java/classes/Aluno.java
+++ src/java/classes/Aluno.java
@@ -1,14 +1,12 @@
 package classes;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Temporal;
 
 @Entity
 public class Aluno implements Serializable{
--- src/java/classes/Ocorrencia.java
+++ src/java/classes/Ocorrencia.java
@@ -1,5 +1,6 @@
 package classes;
 
+import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Temporal;
 
 @Entity
-public class Ocorrencia {
+public class Ocorrencia implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
